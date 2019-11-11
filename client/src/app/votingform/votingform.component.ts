3a20aebed073e0aeaab1fc313edfea8d51eed222
utkarshsaigal <saigalutkarsh@gmail.com>
2019-11-11 15:58:39 +0530

    umessage

--- client/src/app/votingform/votingform.component.ts
+++ client/src/app/votingform/votingform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-votingform',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotingformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us :UsersService) { }
 
   ngOnInit() {
   }
